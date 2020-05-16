4c54c8a77cd7aa5c62ef14f8c1a990eb32704083
José Montero <jokmeil@gmail.com>
2020-05-16 20:07:25 +0200

    kata2 solucion inicial

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,11 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+    password = ''
+    characters = string.ascii_letters + string.digits + string.punctuation
 
-    return ""
+    for valor in range(passLen):
+        char = random.choice(characters)
+        password += char
+
+    return password
